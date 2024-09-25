9f28acc3cb4a5926e5fba050cb3e87a42ead2484
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2024-09-24 22:55:22 -0300

    Add files via upload

--- bissecao2.py
+++ bissecao2.py
@@ -11,16 +11,30 @@ def bissecao(f, a, b, tol=1e-10):
             a = c
     return (a + b) / 2
 
+def check_intervalos(f):
+    a = 1
+    b = 2
+
+    return a, b
+
 # Entrando com a função e o intervalo
 func_str = input("Digite a função f(x): ")
-a = float(input("Digite o limite inferior do intervalo (a): "))
-b = float(input("Digite o limite superior do intervalo (b): "))
+a = input("Digite o limite inferior do intervalo (a): ")
+b = input("Digite o limite superior do intervalo (b): ")
 tol = float(input("Digite a precisão desejada: "))
 
 # Transformando a string em função utilizável
 f = lambda x: eval(func_str)
 
+# Verificando se o intervalo foi definido
+if a == b:
+    # Checar o intervalo
+    a, b = check_intervalos(f)
+else:
+    a = float(a)
+    b = float(b)
+
 # Calculando a raiz
 raiz = bissecao(f, a, b, tol)
-print(f"A raiz aproximada é: {raiz:}")
 
+print(f"A raiz aproximada é: {raiz}")
